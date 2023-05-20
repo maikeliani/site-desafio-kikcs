68ef4f1a69942e60d0c23677fb3c752e71c0d61a
maikeliani <95245954+maikeliani@users.noreply.github.com>
2023-05-20 01:53:24 -0300

    salvo
    .

---
+++ teste centraliacao DEU CERTOO/scripts/script1.js
@@ -0,0 +1,8 @@
+function MostraCoelho() {
+    var coelho = document.getElementById("coelho").style.display='none' ;   
+}
+
+function EscondeTodosOsParagrafos(){
+    var paragrafo = document.getElementsByTagName('p').style.display='none';
+}
+  
